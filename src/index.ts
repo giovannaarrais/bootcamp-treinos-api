a731adee866eebf3a02f91afef4dca05b8bd721e
giovannaarrais <giovannaarrais13@gmail.com>
2026-04-12 17:20:25 -0300

    :wrench: chore: add zod and fastify-type-provider-zod setup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
 import Fastify from "fastify";
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod';
+import z from "zod";
 
-const fastify = Fastify({
+
+const app = Fastify({
 	logger: true,
+})
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.withTypeProvider<ZodTypeProvider>().route({
+	method: 'GET',
+	url: "/",
+	schema: {
+		response: {
+			200: z.object({ // quando a rota retornar 200 ira retornar essa tipagem
+				message: z.string(),
+			})
+		}
+	},
+	// aqui sao os dados que a rota ira retornar
+	handler: () => {
+		return {
+			message: "Hello World!"
+		}
+	}
 });
 
-fastify.get("/", function (request, reply) {
-	reply.send({ hello: "world" });
-});
-
-fastify.listen({ port: Number(process.env.PORT) || 8081 }, function (err, address) {
+app.listen({ port: Number(process.env.PORT) || 8081 }, function (err) {
 	if (err) {
-		fastify.log.error(err);
+		app.log.error(err);
 		process.exit(1);
 	}
 });
